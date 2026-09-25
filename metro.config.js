fa1edc841cdafb068860ef1fd444045583358796
carlitomayn <carlosjavierluque@gmail.com>
2026-09-25 12:15:44 +0200

    Login

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+if (!config.resolver.assetExts.includes('ufw')) {
+  config.resolver.assetExts.push('ufw');
+}
+
+module.exports = config;
